ef910e20df59b60f13dd8822f5c16d9c0122b1a5
Karma-Chuki <109060808+Karma-Chuki@users.noreply.github.com>
2024-08-12 00:01:57 +0530

    Add files via upload

---
+++ myquizapplication/QuestionAnswer.java
@@ -0,0 +1,22 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+    public static String question[] ={
+        "Which company owns the android?",
+        "Who invented Android?",
+        "Which software is used for iOS app development?",
+    };
+
+    public static String choice[][] = {
+            {"Google","Apple","Nokia","Microsoft"},
+            {"Charles Babbage","Alan Turing","Andy Rubin","Bill Gates"},
+            {"Xcode","Android Studio","intellij","Code blocks"},
+    };
+
+    public static String correctAnswers[] ={
+            "Google",
+            "Andy Rubin",
+            "Xcode",
+    };
+
+}
